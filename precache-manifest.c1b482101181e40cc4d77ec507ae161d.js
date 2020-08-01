daafad4726fff2acfedf6eaf9e7bf8b905503e40
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2020-08-01 21:49:40 +0530

    1st try

---
+++ precache-manifest.c1b482101181e40cc4d77ec507ae161d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45471ac98312d09070d84c4c2d46d126",
+    "url": "/DairyReactRedux/index.html"
+  },
+  {
+    "revision": "b3ff49bce47a1c897f02",
+    "url": "/DairyReactRedux/static/css/2.f7daa5b4.chunk.css"
+  },
+  {
+    "revision": "ea2a93859aa2744656fa",
+    "url": "/DairyReactRedux/static/css/main.8acfa7dc.chunk.css"
+  },
+  {
+    "revision": "b3ff49bce47a1c897f02",
+    "url": "/DairyReactRedux/static/js/2.95c4d561.chunk.js"
+  },
+  {
+    "revision": "ce74b3fb49c68c74fdbba237f5bfdad0",
+    "url": "/DairyReactRedux/static/js/2.95c4d561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2a93859aa2744656fa",
+    "url": "/DairyReactRedux/static/js/main.b2469bc0.chunk.js"
+  },
+  {
+    "revision": "c6b85d3d459488ce00cf",
+    "url": "/DairyReactRedux/static/js/runtime-main.e3e3300a.js"
+  },
+  {
+    "revision": "b84e11f2244e4f7b42ee5756c12d9e7c",
+    "url": "/DairyReactRedux/static/media/login.b84e11f2.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/DairyReactRedux/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/DairyReactRedux/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/DairyReactRedux/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/DairyReactRedux/static/media/notification.c0d3c94c.eot"
+  }
+]);
